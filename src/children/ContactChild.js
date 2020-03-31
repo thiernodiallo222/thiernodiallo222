24906885f8d0a7734f6ce5693134992fa0d42128
thiernodiallo222 <thiernodiallo864@gmail.com>
2020-03-31 16:44:32 -0400

    added a contact page

---
+++ src/children/ContactChild.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+
+const ContactChild = () => {
+    const [user,  setUser] = useState({ name: "", email: "", message: "" });
+    const handleChange = (e) => {
+        setUser({  ...user, [e.target.name]:  e.target.value });
+    }
+    return (
+        <div className="contact-container">
+            <div className="contact-container insider-container">
+            <h1 className="contact-title">CONTACT ME</h1>
+            <input text="text" name="name" value={ user.name } placeholder="PLEASE ENTER YOUR FULL NAME" onChange={handleChange}/>
+            <input text="text" name="email" value={ user.email } placeholder="PLEASE ENTER YOUR EMAIL ADDRESS" onChange={handleChange} />
+            <textarea type="text" value={ user.message } name="message" onChange={handleChange}></textarea>
+            <button text="text">Submit</button>
+            </div>
+        </div>
+    )
+}
+
+export default ContactChild;
