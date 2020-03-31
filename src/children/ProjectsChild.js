dc43fbcaaa8bdb97fbc34fbedd6606a0cea1fb62
thiernodiallo222 <thiernodiallo864@gmail.com>
2020-03-31 19:53:52 -0400

    started the projects page

--- src/children/ProjectsChild.js
+++ src/children/ProjectsChild.js
@@ -1,13 +1,31 @@
 import React from 'react'
-import ProjectsCard from '../greatChildren/ProjectsCard';
+// import ProjectsCard from '../greatChildren/ProjectsCard';
 import './children.css';
 
 const ProjectsChild = () => {
     return (
         <div>
            
-            <ProjectsCard />
-         
+            <div className="project-page">
+
+                <div className="insider-lines">
+                    <div className="box-long">   box one  </div>
+                    <div className="box-med">  box-two   </div>
+                    <div className="box-small">  box-three </div>
+                </div>
+
+                    <div className="insider-lines">
+                    <div className="box-small"> box-four </div>
+                    <div className="box-med"> box-five</div>
+                    <div className ="box-long"> box-six</div>
+                </div>
+
+                <div className="insider-lines">
+                    <div className="box-long"> box-seven</div>
+                    <div className="box-med"> box-seven</div>
+                    <div className ="box-small"> box-seven</div>
+                </div>
+            </div>     
         </div>
     )
 }
