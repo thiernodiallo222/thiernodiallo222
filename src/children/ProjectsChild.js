0e63fef8dd365e60722b0edd4cb0d9073bbcf74c
Thierno Diallo <thiernodiallo864@gmail.com>
2021-02-19 03:47:43 -0500

    jc

--- src/children/ProjectsChild.js
+++ src/children/ProjectsChild.js
@@ -11,21 +11,21 @@ const ProjectsChild = () => {
 
                 <div className="insider-lines">
                     <div className="box-long">
-                    <HashLink  className="abdoul" to="/https://african-marketplace-front-end-rssjtm.netlify.app/">Sauti-Africa's Market</HashLink>
+                    <HashLink  className="abdoul" to="/https://african-marketplace-front-end-rssjtm.netlify.app">Sauti-Africa's Market</HashLink>
                     
                      </div>
                  
                     <div className="box-med"> 
-                    <HashLink className="abdoul" to="https://sauti-africa.herokuapp.com/">Sauti-Africa Backend</HashLink>
+                    <HashLink className="abdoul" to="https://sauti-africa.herokuapp.com">Sauti-Africa Backend</HashLink>
                     </div>
                     <div className="box-small"> 
-                    <HashLink className="abdoul" to="https://quizzical-edison-40952e.netlify.app/">Game of life</HashLink>
+                    <HashLink className="abdoul" to="https://quizzical-edison-40952e.netlify.app">Game of life</HashLink>
                     </div>
                 </div>
-
+                                               
                 <div className="insider-lines">
                     <div className="box-small">
-                    <HashLink className="abdoul" to="https://african-marketplace-front-end-rssjtm.netlify.app/">Scheduler for VBB Project</HashLink>
+                    <HashLink className="abdoul" to="https://african-marketplace-front-end-rssjtm.netlify.app">Scheduler for VBB Project</HashLink>
                      </div>
                     <div className="box-med"> coming soon </div>
                     <div className ="box-long"> coming soon </div>
