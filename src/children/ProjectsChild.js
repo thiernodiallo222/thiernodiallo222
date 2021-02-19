32ab7a237d56fc46e77704d3c313499c583d8b9f
Thierno Diallo <thiernodiallo864@gmail.com>
2021-02-19 03:22:58 -0500

    added links

--- src/children/ProjectsChild.js
+++ src/children/ProjectsChild.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { HashLink as Link } from 'react-router-hash-link';
-import Redirect from '../components/Redirect';
 import { Route} from 'react-router-dom';
 import './children.scss';
 
@@ -12,21 +11,21 @@ const ProjectsChild = () => {
 
                 <div className="insider-lines">
                     <div className="box-long">
-                    <Link to="/https://african-marketplace-front-end-rssjtm.netlify.app/">Sauti-Africa's Market</Link>
+                    <Link  className="abdoul" to="/https://african-marketplace-front-end-rssjtm.netlify.app/">Sauti-Africa's Market</Link>
                     
                      </div>
                  
                     <div className="box-med"> 
-                    <Link to="https://sauti-africa.herokuapp.com/">Sauti'Africa Backend</Link>
+                    <Link className="abdoul" to="https://sauti-africa.herokuapp.com/">Sauti-Africa Backend</Link>
                     </div>
                     <div className="box-small"> 
-                    <Link to="https://quizzical-edison-40952e.netlify.app/">Game of life</Link>
+                    <Link className="abdoul" to="https://quizzical-edison-40952e.netlify.app/">Game of life</Link>
                     </div>
                 </div>
 
                 <div className="insider-lines">
                     <div className="box-small">
-                    <Link to="https://african-marketplace-front-end-rssjtm.netlify.app/">Scheduler for VBB Project</Link>
+                    <Link className="abdoul" to="https://african-marketplace-front-end-rssjtm.netlify.app/">Scheduler for VBB Project</Link>
                      </div>
                     <div className="box-med"> coming soon </div>
                     <div className ="box-long"> coming soon </div>
