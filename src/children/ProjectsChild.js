4b59ed9d8eafc684b20d0810210b3a86556037d7
Thierno Diallo <thiernodiallo864@gmail.com>
2021-02-19 02:59:56 -0500

    added hash link

--- src/children/ProjectsChild.js
+++ src/children/ProjectsChild.js
@@ -1,12 +1,9 @@
 import React from 'react'
-// import { HashLink } from 'react-router-hash-link';
-import { genericHashLink } from 'react-router-hash-link';
-import GatsbyLink from 'gatsby-link';
+import { HashLink as Link } from 'react-router-hash-link';
 import Redirect from '../components/Redirect';
 import { Route, Link } from 'react-router-dom';
 import './children.scss';
 
-const MyHashLink = genericHashLink(GatsbyLink);
 
 const ProjectsChild = () => {
     return (
@@ -15,7 +12,8 @@ const ProjectsChild = () => {
 
                 <div className="insider-lines">
                     <div className="box-long">
-                    <MyHashLink to="https://african-marketplace-front-end-rssjtm.netlify.app/">Sauti African market</MyHashLink>
+                    <Link to="https://african-marketplace-front-end-rssjtm.netlify.app/">Sauti'Africa market</Link>
+                    {/* <MyHashLink to="https://african-marketplace-front-end-rssjtm.netlify.app/">Sauti African market</MyHashLink> */}
                                {/* <Link to ="/better-professor" className="better-professor"> African market Place </Link>
                           <Route path="/better-professor" component={ Redirect }
                                loc="https://african-marketplace-front-end-rssjtm.netlify.app/"/>     */}
